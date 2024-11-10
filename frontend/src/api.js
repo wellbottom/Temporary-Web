b3c10626f68a040a33cf6bfcd8239e757588c866
wellbottom <125947127+wellbottom@users.noreply.github.com>
2024-11-11 01:43:19 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,11 +2,11 @@
 
 import axios from "axios"
 import {ACCESS_TOKEN} from "./constant"
-
+const apiUrl = "/choreo-apis/temporary-webapp/backend/v1";
 const api = axios.create({
     baseURL:import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
-const apiUrl = "/choreo-apis/temporary-webapp/backend/v1";
+
 api.interceptors.request.use(
     (config) =>{
         const token = localStorage.getItem(ACCESS_TOKEN);
